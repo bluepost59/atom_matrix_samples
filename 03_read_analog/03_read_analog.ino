50cc5a94aede0b4d9f3dbd69a7971af1c610dd1d
bluepost59 <kaput.kat@gmail.com>
2020-05-06 22:50:16 +0900

    read_analogを追加

---
+++ 03_read_analog/03_read_analog.ino
@@ -0,0 +1,46 @@
+#include "M5Atom.h"
+
+const int pin_temperature = 25;
+
+const int B = 4275;
+const int R0 = 100000;
+
+float read_temperature(int pin_number);
+
+void setup()
+{
+    M5.begin(true, false, true);
+    Serial.print("\nhello\n");
+}
+
+void loop()
+{
+    float temperture = read_temperature(pin_temperature);
+
+    for (uint32_t i = 0; i < 25; i++)
+    {
+        M5.dis.drawpix(i, 0xf00000);
+        delay(25);
+    }
+    // delay(500);
+
+    for (uint32_t i = 0; i < 25; i++)
+    {
+        M5.dis.drawpix(i, 0x000000);
+        delay(25);
+    }
+    // delay(500);
+}
+
+float read_temperature(int pin_number){
+    int raw_temperature = analogRead(pin_number);
+    float R = 1023.0 / (raw_temperature*1024.0/4096.0) -1.0;
+
+    float temperature = 1.0 / (log(R)/ B + 1 / 298.15) - 275.15;
+
+    // Serial.println(R);
+    // Serial.println(raw_temperature);
+    Serial.println(temperature);
+
+    return temperature;
+}
